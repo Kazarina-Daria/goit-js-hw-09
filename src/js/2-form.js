040af373956d74e5c223a12ebbd0c1b8cc5cf195
Daria Kazarina <kdasa033@gmail.com>
2025-10-05 16:27:11 +0200

    task 2 fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,27 +1,31 @@
-const formData = {
-     email: "",
-     message: "",
+const formData = {email: '', message: ''};
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', handlerText);
+
+function handlerText(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
-let data = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+if (savedData) {
+  form.elements.email.value = savedData.email || '';
+  form.elements.message.value = savedData.message || '';
+  formData.email = savedData.email || "";
+    formData.message = savedData.message || "";
+};
 
-const form = document.querySelector(".feedback-form");
-const STORAGE_KEY = "feedback-form-state";
-const email = form.querySelector(".input-email");
+form.addEventListener('submit', handlersubmit);
+function handlersubmit(e) {
+  e.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 
-
-
-form.addEventListener("input", handlerEmailText);
-function handlerEmailText (e){
-  const emailMessage = e.target.value;
-  localStorage.setItem(STORAGE_KEY, emailMessage);
 }
-
-form.addEventListener("submit", handlersubmit);
-
-function handlersubmit(e){
-    e.preventDefault();
-    e.currenTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-      formData = {};
-};
